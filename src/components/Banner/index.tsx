b88de399902178ac2388569db38a72202261dccf
tanatut12 <tanatut12@gmail.com>
2025-07-10 14:14:30 +0700

    fix banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -15,7 +15,7 @@ const Banner = () => {
         priority
       />
       <div className="absolute top-0 left-0 w-full h-full bg-banner-gradient-1 z-10"></div>
-      <div className="absolute -top-3 right-4 laptop:right-[60px] laptop-lg:right-[90px] app:right-[400px] flex items-center justify-center h-full z-20">
+      <div className="absolute -top-3 right-4 laptop:right-[60px] laptop-lg:right-[390px] app:right-[400px] flex items-center justify-center h-full z-20">
         <BannerCover />
       </div>
     </div>
